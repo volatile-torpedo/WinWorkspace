dccd2d04d86d2a342f2e4c7d8ec6be794bc6fdf7
volatile-torpedo <thisisme@voltol.com>
2024-04-17 21:45:20 -0700

    let's try this again

--- Hydrate-This.ps1
+++ Hydrate-This.ps1
@@ -284,7 +284,6 @@ $IsThisScriptAnInvokedExpression = ($null -eq $MyInvocation.MyCommand.Path)
 # Determine if this process is being run in an elevated process; tested with Windows sudo
 $IsElevated = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
 
-
 # Loop until a valid input is received
 $DoIt = $false
 
